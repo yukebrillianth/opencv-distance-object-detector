8a5c9401d5c94e83cf59229b70aabc26b958c719
yukebrillianth <yukebrilliant@gmail.com>
2024-10-23 23:43:29 +0700

    fix: get largest contour area & HSV to YUV

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ using namespace std;
 
 int main()
 {
-    VideoCapture cap(0);
+    // VideoCapture cap("/dev/v4l/by-id/usb-Generic_Integrated_Camera_200901010001-video-index0"); // Integrated camera
+    VideoCapture cap(0); // Iriun webcam
     if (!cap.isOpened())
     {
         cout << "Unable to access camera!" << endl;
@@ -20,40 +21,48 @@ int main()
      * @brief Configure Trackbar window
      *
      */
-    int h[2] = {46, 255}, s[2] = {0, 255}, v[2] = {0, 42};
+    int y[2] = {0, 79}, y[2] = {24, 111}, v[2] = {0, 20};
     namedWindow("Control");
 
-    createTrackbar("Hmin", "Control", &h[0], 255);
-    createTrackbar("Smin", "Control", &s[0], 255);
+    createTrackbar("Hmin", "Control", &y[0], 255);
+    createTrackbar("Smin", "Control", &y[0], 255);
     createTrackbar("Vmin", "Control", &v[0], 255);
 
-    createTrackbar("Hmax", "Control", &h[1], 255);
-    createTrackbar("Smax", "Control", &s[1], 255);
+    createTrackbar("Hmax", "Control", &y[1], 255);
+    createTrackbar("Smax", "Control", &y[1], 255);
     createTrackbar("Vmax", "Control", &v[1], 255);
 
     while (true)
     {
         cap >> frame;
 
-        // Convert BGR to HSV
-        Mat frame_hsv;
-        cvtColor(frame, frame_hsv, COLOR_BGR2HSV);
+        // Convert BGR to yuv
+        Mat frame_yuv;
+        cvtColor(frame, frame_yuv, COLOR_BGR2YUV);
 
         Mat frame_thres;
-        inRange(frame, Scalar(h[0], s[0], v[0]), Scalar(h[1], s[1], v[1]), frame_thres);
+        inRange(frame, Scalar(y[0], y[0], v[0]), Scalar(y[1], y[1], v[1]), frame_thres);
 
         vector<vector<Point>> contours;
         findContours(frame_thres, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
-        for (size_t i = 0; i < contours.size(); i++)
+        if (!contours.empty())
         {
-            double area = contourArea(contours[i]);
+            int largestContourAreaI = 0;
+            double largestArea = contourArea(contours[0]);
 
-            // Ini tergantung ukuran bendanya ges,
-            if (area < 500)
-                continue;
+            // Get largest contour area
+            for (size_t i = 0; i < contours.size(); i++)
+            {
+                double area = contourArea(contours[i]);
 
-            Moments m = moments(contours[i]);
+                if (area > largestArea)
+                {
+                    largestArea = area;
+                    largestContourAreaI = i;
+                }
+            }
+            Moments m = moments(contours[largestContourAreaI]);
             double mArea = m.m00;
             double mX = m.m10;
             double mY = m.m01;
@@ -64,17 +73,17 @@ int main()
             float radius;
 
             // Hitung min enclosing circle
-            minEnclosingCircle(contours[i], circleLine, radius);
+            minEnclosingCircle(contours[largestContourAreaI], circleLine, radius);
 
             float distance = (200 * 22) / radius;
-            cout << radius << endl;
+            cout << "Radius: " << radius << endl;
 
             // gambar enclosing circle
             circle(frame, circleLine, (int)radius, Scalar(0, 255, 255), 2);
 
             // show info
             string distanceText = "Distance: " + to_string(int(distance)) + " cm";
-            string areaText = "Area: " + to_string(int(area));
+            string areaText = "Area: " + to_string(int(largestArea));
             putText(frame, distanceText, Point(circleLine.x - radius, circleLine.y - radius - 10),
                     FONT_HERSHEY_SIMPLEX, 0.5, Scalar(0, 255, 0), 2);
             putText(frame, areaText, Point(circleLine.x - radius, circleLine.y - radius - 30),
