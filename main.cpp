09bf9f920c67f6449cc080dac9ba54e869bb5478
yukebrillianth <yukebrilliant@gmail.com>
2024-10-24 20:14:35 +0700

    feat: add polynomial regression

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/videoio.hpp>
 #include <vector>
+#include <math.h>
 
 using namespace cv;
 using namespace std;
+
+float calculateDistance(float radius)
+{
+    // const double a = 2.0868157930708398, b = -4.3881898257058580, c = 3.7050037050199380, d = -1.0918760918813372;
+
+    // return ((a * pow(10, 2)) * radius) + ((b * pow(10, 0)) * radius) + ((c * pow(10, -2)) * radius) + ((d * pow(10, -4)) * radius);
+
+    // Langsung copy dari calc arachnoid nyah
+    float terms[] = {
+        1.4179837576930566e+002,
+        -2.0652213774139256e+000,
+        1.1648757199107242e-002,
+        -2.1895026422167324e-005};
+
+    float t = 1;
+    float r = 0;
+    for (float c : terms)
+    {
+        r += c * t;
+        t *= radius;
+    }
+    return r;
+}
 
 int main()
 {
@@ -21,15 +45,15 @@ int main()
      * @brief Configure Trackbar window
      *
      */
-    int y[2] = {0, 79}, y[2] = {24, 111}, v[2] = {0, 20};
+    int y[2] = {15, 165}, u[2] = {59, 168}, v[2] = {0, 50};
     namedWindow("Control");
 
-    createTrackbar("Hmin", "Control", &y[0], 255);
-    createTrackbar("Smin", "Control", &y[0], 255);
+    createTrackbar("Ymin", "Control", &y[0], 255);
+    createTrackbar("Umin", "Control", &u[0], 255);
     createTrackbar("Vmin", "Control", &v[0], 255);
 
-    createTrackbar("Hmax", "Control", &y[1], 255);
-    createTrackbar("Smax", "Control", &y[1], 255);
+    createTrackbar("Ymax", "Control", &y[1], 255);
+    createTrackbar("Umax", "Control", &u[1], 255);
     createTrackbar("Vmax", "Control", &v[1], 255);
 
     while (true)
@@ -41,7 +65,7 @@ int main()
         cvtColor(frame, frame_yuv, COLOR_BGR2YUV);
 
         Mat frame_thres;
-        inRange(frame, Scalar(y[0], y[0], v[0]), Scalar(y[1], y[1], v[1]), frame_thres);
+        inRange(frame, Scalar(y[0], u[0], v[0]), Scalar(y[1], u[1], v[1]), frame_thres);
 
         vector<vector<Point>> contours;
         findContours(frame_thres, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
@@ -75,7 +99,8 @@ int main()
             // Hitung min enclosing circle
             minEnclosingCircle(contours[largestContourAreaI], circleLine, radius);
 
-            float distance = (200 * 22) / radius;
+            // float distance = (200 * 22) / radius;
+            float distance = calculateDistance(radius);
             cout << "Radius: " << radius << endl;
 
             // gambar enclosing circle
